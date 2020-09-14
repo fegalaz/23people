591d0816f7f684274abc94c66e8ffff8a338d1cb
Felipe Galaz <alonso.molina@amaris.com>
2020-09-14 16:40:27 -0300

    OAuth2 y JWT

---
+++ src/main/java/com/auth/BootLauncher.java
@@ -0,0 +1,12 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+public class BootLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(BootLauncher.class, args);
+    }
+}
